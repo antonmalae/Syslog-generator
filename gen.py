484a291c6547dc1711ac7edb2f2956657e07d94f
FatTony <45072917+antonmalae@users.noreply.github.com>
2024-05-15 08:33:46 +0000

    add background mode

--- gen.py
+++ gen.py
@@ -3,6 +3,9 @@ import string
 import random
 import threading
 import time
+import daemon
+import daemon.pidfile
+import os
 from datetime import datetime
 import sys
 from faker import Faker
@@ -12,7 +15,7 @@ fake = Faker()
 def generate_random_message():
     priority = random.randint(0, 7) 
     current_time = datetime.now().strftime("%b %d %H:%M:%S") 
-    hostname = "MyComputerName" 
+    hostname = "GenerateHostname" 
     words = [fake.word() for _ in range(5)] 
     message = ' '.join(words)
     log_type = random.choice(["alert", "info", "warn", "event"])
@@ -48,8 +51,8 @@ def send_from_file(ip, port, file_path):
             time.sleep(1) 
 
 def main():
-    if len(sys.argv) < 4:
-        print("Usage: python script.py [IP_ADDRESS] [PORT] [EVENTS_PER_SECOND] [MODE]")
+    if len(sys.argv) < 5:
+        print("Usage: python script.py [IP_ADDRESS] [PORT] [EVENTS_PER_SECOND] [MODE] [BACKGROUND (optional)]")
         sys.exit(1)
 
     ip_address = sys.argv[1]
@@ -60,13 +63,33 @@ def main():
     if events_per_second < 1 or events_per_second > 1000:
         print("EVENTS_PER_SECOND must be between 1 and 1000")
         sys.exit(1)
+    
+    if mode not in ['random', 'file']:
+        print("Invalid mode. Please use 'random' or 'file'")
+        sys.exit(1)
+
+    background_mode = False
+    if len(sys.argv) > 5 and sys.argv[5].lower() == 'background':
+        background_mode = True
 
     if mode == 'random':
-        sys_thread = threading.Thread(target=send_periodic_syslog, args=(ip_address, port_number, events_per_second))
-        sys_thread.start()
+        if background_mode:
+            with daemon.DaemonContext():
+                sys_thread = threading.Thread(target=send_periodic_syslog, args=(ip_address, port_number, events_per_second))
+                sys_thread.start()
+        else:
+            sys_thread = threading.Thread(target=send_periodic_syslog, args=(ip_address, port_number, events_per_second))
+            sys_thread.start()
     elif mode == 'file':
-        file_path = input("Enter the path to the file: ")
-        send_from_file(ip_address, port_number, file_path)
+        if len(sys.argv) < 6:
+            print("Usage: python script.py [IP_ADDRESS] [PORT] [EVENTS_PER_SECOND] file [FILE_PATH] [BACKGROUND (optional)]")
+            sys.exit(1)
+        file_path = sys.argv[5]
+        if background_mode:
+            with daemon.DaemonContext():
+                send_from_file(ip_address, port_number, file_path)
+        else:
+            send_from_file(ip_address, port_number, file_path)
     else:
         print("Invalid mode. Please use 'random' or 'file'")
         sys.exit(1)
